b1fc916ca58aa2eab1f73a74390cfa700368750c
miguecoder <mmarqzm@gmail.com>
2022-07-05 22:37:43 -0500

    Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,23 +17,23 @@ class FileStorage:
     """Class that serializes instances to a JSON file
     and deserializes JSON file to instances:"""
     __file_path = "file.json"
-    __object = {}
+    __objects = {}
 
     def all(self):
         """Public instance methods that returns the dictionary __objects"""
-        return self.__object
+        return self.__objects
 
     def new(self, obj):
         """eturns the dictionary __objects sets
         in __objects the obj with key <obj class name>.id"""
         key = f"{type(obj).__name__}.{obj.id}"
-        self.__object[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Public instance methods serializes
         __objects to the JSON file (path: __file_path)"""
         with open(self.__file_path, mode="w", encoding="UTF-8") as file:
-            data = {k: v.to_dict() for k, v in self.__object.items()}
+            data = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(data, file)
 
     def reload(self):
@@ -42,4 +42,4 @@ class FileStorage:
             with open(self.__file_path, mode="r", encoding="UTF-8") as file:
                 dic = json.loads(file.read())
                 for k, v in dic.items():
-                    self.__object[k] = eval(v["__class__"])(**v)
+                    self.__objects[k] = eval(v["__class__"])(**v)
