e0119ffedff95d8dc103bc1cb78bfbcec6d648cf
miguecoder <mmarqzm@gmail.com>
2022-07-05 21:17:11 -0500

    Update models/base_model.py to __init__(self, *args, **kwargs):

--- models/base_model.py
+++ models/base_model.py
@@ -23,12 +23,7 @@ class BaseModel:
                 else:
                     if key != '__class__':
                         setattr(self, key, value)
-        """
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        """
+
     def __str__(self):
         """Methods that print: [<class name>] (<self.id>) <self.__dict__>"""
 
