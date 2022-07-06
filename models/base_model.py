7aaf47bd7b8938bd2ada9524d16863c430bdb3f9
miguecoder <mmarqzm@gmail.com>
2022-07-05 21:01:02 -0500

    Update models/base_model.py to __init__(self, *args, **kwargs):

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,31 @@
 import uuid
 from datetime import datetime
 
+format = '%Y-%m-%dT%H:%M:%S.%f'
+
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor of class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, format)
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(value, format)
+                else:
+                    if key != '__class__':
+                        setattr(self, key, value)
+        """
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        """
     def __str__(self):
         """Methods that print: [<class name>] (<self.id>) <self.__dict__>"""
 
